e4327626d6d69b510d2af41ad646989a66e16b5e
hjyoon02 <hjyoon02@postech.ac.kr>
2022-04-09 02:08:06 +0900

    Ground normal changed

--- CSED451/Fortress/Ground.cpp
+++ CSED451/Fortress/Ground.cpp
@@ -25,9 +25,9 @@ Ground::Ground() {
 			uvs.push_back(glm::vec2(0, 0));
 			uvs.push_back(glm::vec2(0, 0));
 			uvs.push_back(glm::vec2(0, 0));
-			normals.push_back(glm::vec3(0, 0, 1));
-			normals.push_back(glm::vec3(0, 0, 1));
-			normals.push_back(glm::vec3(0, 0, 1));
+			normals.push_back(glm::vec3(0, 1, 0));
+			normals.push_back(glm::vec3(0, 1, 0));
+			normals.push_back(glm::vec3(0, 1, 0));
 		}
 	}
 
